85d57ee8b68a9f5ceb70bbc475d6e7f3acfa13c1
Jin Lee <jin.lee@bydeluxe.com>
2016-01-31 17:59:45 -0800

    create todo list and todo items, migration

---
+++ db/migrate/20160201015530_create_todo_item.rb
@@ -0,0 +1,9 @@
+class CreateTodoItem < ActiveRecord::Migration[5.0]
+  def change
+    create_table :todo_items do |t|
+      t.references :todo_list
+      t.string :description
+      t.timestamps
+    end
+  end
+end
