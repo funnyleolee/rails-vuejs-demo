ac9052ef9d8856b6fe2353dee3ffb3c78b967b2e
Jin Lee <jin.lee@bydeluxe.com>
2016-01-31 23:20:19 -0800

    remove turbolinks, screwing up vue

--- app/assets/javascripts/todo_lists.js
+++ app/assets/javascripts/todo_lists.js
@@ -8,15 +8,16 @@ $(document).ready(function() {
         name: '',
         todo_items: []
       },
-      new_todo_description: ''
+      new_todo_item: {
+        description: ''
+      }
     },
 
     methods: {
       addTodoItem: function() {
-        var description = this.new_todo_description.trim()
-        if (description) {
-          this.todo_list.todo_items.push({ description: description });
-          this.new_todo_description = '';
+        if (this.new_todo_item.description.trim()) {
+          this.todo_list.todo_items.push($.extend({}, this.new_todo_item));
+          this.new_todo_item.description = '';
         }
       }
     }
